665c31f049fbc55809c7bda0cc31645b756959e3
@guycreate <guy@domabo.com>
2015-09-01 16:00:55 -0500

    consistency with iopa-udp

--- src/tcpClient.js
+++ src/tcpClient.js
@@ -29,59 +29,71 @@ var iopaContextFactory = require('iopa').context.factory;
  * IOPA TCP CLIENT (GENERIC)  
  * ********************************************************* */
 
- /**
- * Creates a new IOPA Request using a Tcp Url including host and port name
- *
- * @method TcpClient
+/**
+* Creates a new IOPA Request using a Tcp Url including host and port name
+*
+* @method TcpClient
 
- * @parm {object} options not used
- * @parm {string} urlStr url representation of ://127.0.0.1:8200
- * @public
- * @constructor
- */
-function TcpClient(options, clientMessagePipeline) { 
-   events.EventEmitter.call(this);
-  
+* @parm {object} options not used
+* @parm {string} urlStr url representation of ://127.0.0.1:8200
+* @public
+* @constructor
+*/
+function TcpClient(options, clientMessagePipeline) {
+  events.EventEmitter.call(this);
+
   this._options = options;
   this._clientMessagePipeline = clientMessagePipeline;
-  }
+
+  this._connections = {};
+}
 
 util.inherits(TcpClient, events.EventEmitter);
 
- /**
- * Creates a new IOPA Request using a Tcp Url including host and port name
- *
- * @method connect
+/**
+* Creates a new IOPA Request using a Tcp Url including host and port name
+*
+* @method connect
 
- * @parm {object} options not used
- * @parm {string} urlStr url representation of ://127.0.0.1:8200
- * @public
- * @constructor
- */
-TcpClient.prototype.connect = function TcpClient_connect(urlStr){
-     
+* @parm {object} options not used
+* @parm {string} urlStr url representation of ://127.0.0.1:8200
+* @public
+* @constructor
+*/
+TcpClient.prototype.connect = function TcpClient_connect(urlStr) {
+
   var channelContext = iopaContextFactory.createRequest(urlStr, "TCP-CONNECT");
-  
+
   channelContext["tcp._BaseUrl"] = urlStr;
   channelContext["server.CreateRequest"] = TcpClient_CreateRequest.bind(this, channelContext);
-  
+  channelContext["server.IsLocalOrigin"] = true;
+  channelContext["server.IsRequest"] = true;
+  channelContext["iopa.MessageId"] = channelContext["iopa.Seq"];
+
   var that = this;
-  return new Promise(function(resolve, reject){
-      var socket = net.createConnection(
-        channelContext["server.RemotePort"], 
-        channelContext["server.RemoteAddress"],
-        function() {
-          channelContext["server.Id"] = socket.localAddress + ':' + socket.localPort;
-          channelContext["server.SessionId"] = socket.remoteAddress + ':' + socket.remotePort;
-          channelContext["server.RawStream"] = socket;
-          channelContext["server.LocalAddress"] = socket.localAddress;
-          channelContext["server.LocalPort"] = socket.localPort;
-          channelContext["server.RawStream"].on('finish', that._disconnect.bind(that, channelContext, null));
-          channelContext["server.RawStream"].on('error', that._disconnect.bind(that, channelContext)); 
-          resolve(channelContext);
-         });
-     });
- };
+  return new Promise(function (resolve, reject) {
+    var socket = net.createConnection(
+      channelContext["server.RemotePort"],
+      channelContext["server.RemoteAddress"],
+      function () {
+        channelContext["tcp.RawSocket"] = socket;
+
+        channelContext["server.Id"] = socket.localAddress + ':' + socket.localPort;
+        channelContext["server.SessionId"] = socket.remoteAddress + ':' + socket.remotePort;
+        channelContext["server.RawStream"] = socket;
+        channelContext["server.LocalAddress"] = socket.localAddress;
+        channelContext["server.LocalPort"] = socket.localPort;
+        channelContext["server.RawStream"].on('finish', that._disconnect.bind(that, channelContext, null));
+        channelContext["server.RawStream"].on('error', that._disconnect.bind(that, channelContext));
+        channelContext["iopa.Body"] = new iopaStream.OutgoingNoPayloadStream();
+        channelContext["server.SessionId"] = channelContext["server.LocalAddress"] + ":" + channelContext["server.LocalPort"] + "-" + channelContext["server.RemoteAddress"] + ":" + channelContext["server.RemotePort"];
+
+        that._connections[channelContext["server.SessionId"]] = channelContext;
+
+        resolve(channelContext);
+      });
+  });
+};
  
 /**
  * Creates a new IOPA Request using a Tcp Url including host and port name
@@ -93,54 +105,42 @@ TcpClient.prototype.connect = function TcpClient_connect(urlStr){
  * @returns {Promise(context)}
  * @public
  */
-function TcpClient_CreateRequest(channelContext, path, method){
+function TcpClient_CreateRequest(channelContext, path, method) {
   var urlStr = channelContext["tcp._BaseUrl"] + path;
-  var context = iopaContextFactory.createRequest(urlStr, method); 
-   
+  var context = iopaContextFactory.createRequest(urlStr, method);
+
   context["iopa.Body"] = new iopaStream.OutgoingStream();
-  context.response["iopa.Body"] = new iopaStream.IncomingMessageStream();
-  
+
   context["server.TLS"] = channelContext["server.TLS"];
   context["server.RemoteAddress"] = channelContext["server.RemoteAddress"];
-  context["server.RemotePort"] = channelContext["server.RemotePort"] ;
+  context["server.RemotePort"] = channelContext["server.RemotePort"];
   context["server.LocalAddress"] = channelContext["server.LocalAddress"];
-  context["server.LocalPort"] = channelContext["server.LocalPort"]; 
-  context["server.RawStream"] = channelContext["server.RawStream"];    
-  context["server.Id"] = channelContext["server.Id"];  
+  context["server.LocalPort"] = channelContext["server.LocalPort"];
+  context["server.RawStream"] = channelContext["server.RawStream"];
+  context["server.Id"] = channelContext["server.Id"];
   context["server.SessionId"] = channelContext["server.SessionId"];
-   
-  context.response["server.TLS"] = channelContext.response["server.TLS"];    
-  context.response["server.RemoteAddress"] = channelContext.response["server.RemoteAddress"];    
-  context.response["server.RemotePort"] = channelContext.response["server.RemotePort"];    
-  context.response["server.LocalAddress"] = channelContext.response["server.LocalAddress"];    
-  context.response["server.LocalPort"] = channelContext.response["server.LocalPort"];    
-  context.response["server.RawStream"] = channelContext.response["server.RawStream"];  
-  context.response["server.Id"] = channelContext["server.Id"]; 
-  context.response["server.SessionId"] = channelContext["server.SessionId"];
-     
+
   context["server.IsLocalOrigin"] = true;
   context["server.IsRequest"] = true;
-  context.response["server.IsLocalOrigin"] = false;
-  context.response["server.IsRequest"] = false;
-  
-  context["iopa.Body"].on("start", function(){
+
+  context["iopa.Body"].on("start", function () {
     context["server.InProcess"] = true;
   });
-  
-  var that = this; 
- 
-  context["iopa.Body"].on("finish", function(){
-     var ctx = context;
-     ctx["server.InProcess"] = true;
-     return that._clientMessagePipeline(context).then(function(value){
-         ctx["iopa.Events"].emit("server.RequestComplete", ctx);
-         ctx["server.InProcess"] = false;
-         iopaContextFactory.dispose(ctx);
-       that = null;
-       ctx = null;
-     });
+
+  var that = this;
+
+  context["iopa.Body"].on("finish", function () {
+    var ctx = context;
+    ctx["server.InProcess"] = true;
+    return that._clientMessagePipeline(context).then(function (value) {
+      ctx["iopa.Events"].emit("server.RequestComplete", ctx);
+      ctx["server.InProcess"] = false;
+      iopaContextFactory.dispose(ctx);
+      that = null;
+      ctx = null;
+    });
   });
-  
+
   return context;
 };
 
@@ -151,9 +151,27 @@ function TcpClient_CreateRequest(channelContext, path, method){
  * @public
  */
 TcpClient.prototype._disconnect = function TcpClient_disconnect(channelContext, err) {
-    channelContext["iopa.Events"].emit("server.Disconnect");
-    channelContext["iopa.CallCancelledSource"].cancel('server.Disconnect');
-    iopaContextFactory.dispose(channelContext);
+  channelContext["iopa.Events"].emit("server.Disconnect");
+  channelContext["iopa.CallCancelledSource"].cancel('server.Disconnect');
+  channelContext["tcp.RawSocket"].destroy();
+  delete this._connections[channelContext["server.SessionId"]];
+  iopaContextFactory.dispose(channelContext);
 }
+
+/**
+ * @method close
+ * Close all the underlying sockets
+ * 
+ * @returns {Promise()}
+ * @public
+ */
+TcpClient.prototype.close = function TcpClient_close() {
+  for (var key in this._connections)
+    this._disconnect(this._connections[key], null);
+
+  this._connections = {};
+
+  return Promise.resolve(null);
+};
 
 module.exports = TcpClient;
--- src/tcpServer.js
+++ src/tcpServer.js
@@ -49,21 +49,19 @@ function TcpServer(options, serverPipeline, clientPipeline) {
 
   this._options = options;
 
-  if (serverPipeline)
-  {
+  if (serverPipeline) {
     this._appFunc = serverPipeline;
     this.on('data', this._invoke.bind(this));
   }
-  
-  if (clientPipeline)
-  {
+
+  if (clientPipeline) {
     this._tcpClient = new TcpClient(options, clientPipeline);
     this._clientMessagePipeline = clientPipeline;
   }
-  
+
   this._connections = {};
-  
- }
+
+}
 
 util.inherits(TcpServer, events.EventEmitter)
 
@@ -76,30 +74,29 @@ util.inherits(TcpServer, events.EventEmitter)
  * @returns {Promise} 
  * @public
  */
- TcpServer.prototype.listen = function TcpServer_listen(port, address) {
- 
-    if (port == undefined) 
-    {
-     port = 0;
-    }
-   
+TcpServer.prototype.listen = function TcpServer_listen(port, address) {
+
+  if (port == undefined) {
+    port = 0;
+  }
+
   if (this._tcp)
-       return Promise.reject('Already listening');
+    return Promise.reject('Already listening');
 
-    var that = this;
+  var that = this;
 
-    this._tcp = net.createServer();
-    this._tcp.on("connection", this._onConnection.bind(this));
-    this._port = port;
-    this._address = address;
+  this._tcp = net.createServer();
+  this._tcp.on("connection", this._onConnection.bind(this));
+  this._port = port;
+  this._address = address;
 
-    return this._tcp.listenAsync(port, address || '0.0.0.0').then(function(){
-          that._linfo = that._tcp.address();
-          that._port = that._linfo.port;
-          that._address = that._linfo.address;
-          
-          return Promise.resolve(that._linfo);
-    });
+  return this._tcp.listenAsync(port, address || '0.0.0.0').then(function () {
+    that._linfo = that._tcp.address();
+    that._port = that._linfo.port;
+    that._address = that._linfo.address;
+
+    return Promise.resolve(that._linfo);
+  });
 };
 
 Object.defineProperty(TcpServer.prototype, "port", { get: function () { return this._port; } });
@@ -107,8 +104,8 @@ Object.defineProperty(TcpServer.prototype, "address", { get: function () { retur
 
 TcpServer.prototype._onConnection = function TcpServer_onConnection(socket) {
   var context = iopaContextFactory.createContext();
-   context["iopa.Method"] = "TCP-CONNECT";
- 
+  context["iopa.Method"] = "TCP-CONNECT";
+
   context["server.TLS"] = false;
   context["server.RemoteAddress"] = socket.remoteAddress;
   context["server.RemotePort"] = socket.remotePort;
@@ -118,7 +115,7 @@ TcpServer.prototype._onConnection = function TcpServer_onConnection(socket) {
   context["server.IsLocalOrigin"] = false;
   context["server.IsRequest"] = true;
   context["server.SessionId"] = socket.remoteAddress + ':' + socket.remotePort;
-  
+
   var response = context.response;
   response["server.TLS"] = context["server.TLS"];
   response["server.RemoteAddress"] = context["server.RemoteAddress"];
@@ -131,39 +128,38 @@ TcpServer.prototype._onConnection = function TcpServer_onConnection(socket) {
 
   context["server.InProcess"] = false;
   socket.on("close", this._onDisconnect.bind(this, context));
-  
+
   this._connections[context["server.SessionId"]] = socket;
   this.emit("data", context);
 };
 
-TcpServer.prototype._onDisconnect = function TcpServer_onDisconnect(context) {    
+TcpServer.prototype._onDisconnect = function TcpServer_onDisconnect(context) {
   delete this._connections[context["server.SessionId"]];
   if (context["server.RawStream"])
     context["server.RawStream"].removeAllListeners('finish');
-   
-    setTimeout(function() {
-        iopaContextFactory.dispose(context);
-         }, 1000);
-    
-   if (context["server.InProcess"])
-   {
-      context["iopa.Events"].emit("server.Disconnect");
-      context["iopa.CallCancelledSource"].cancel('server.Disconnect');
-   }
+
+  setTimeout(function () {
+    iopaContextFactory.dispose(context);
+  }, 1000);
+
+  if (context["server.InProcess"]) {
+    context["iopa.Events"].emit("server.Disconnect");
+    context["iopa.CallCancelledSource"].cancel('server.Disconnect');
+  }
 };
 
 TcpServer.prototype._invoke = function TcpServer_invoke(context) {
   context["server.CreateRequest"] = this.createResponseRequest.bind(this, context);
- 
+
   var that = this;
   var ctx = context;
-   context["server.InProcess"] = true;
-  return this._appFunc(context).then(function(value){
-     ctx["server.InProcess"] = false;
-     iopaContextFactory.dispose(ctx);
-     that = null;
-     ctx = null;
-     return value;
+  context["server.InProcess"] = true;
+  return this._appFunc(context).then(function (value) {
+    ctx["server.InProcess"] = false;
+    iopaContextFactory.dispose(ctx);
+    that = null;
+    ctx = null;
+    return value;
   });
 };
 
@@ -176,8 +172,8 @@ TcpServer.prototype._invoke = function TcpServer_invoke(context) {
  * @returns {Promise(context)}
  * @public
  */
-TcpServer.prototype.connect = function TcpServer_connect(urlStr){
-    return this._tcpClient.connect(urlStr);
+TcpServer.prototype.connect = function TcpServer_connect(urlStr) {
+  return this._tcpClient.connect(urlStr);
 };
 
 /**
@@ -190,39 +186,40 @@ TcpServer.prototype.connect = function TcpServer_connect(urlStr){
  * @returns {Promise(context)}
  * @public
  */
-TcpServer.prototype.createResponseRequest = function TcpServer_createResponseRequest(originalContext, path, method){
-  var urlStr = originalContext["iopa.Scheme"] + 
-   "://" +
-   originalContext["server.RemoteAddress"] + ":" + originalContext["server.RemotePort"] + 
-  originalContext["iopa.PathBase"] +
-  originalContext["iopa.Path"] + path;
-  
-  var context = iopaContextFactory.createRequest(urlStr, method); 
- 
+TcpServer.prototype.createResponseRequest = function TcpServer_createResponseRequest(originalContext, path, method) {
+  var urlStr = originalContext["iopa.Scheme"] +
+    "://" +
+    originalContext["server.RemoteAddress"] + ":" + originalContext["server.RemotePort"] +
+    originalContext["iopa.PathBase"] +
+    originalContext["iopa.Path"] + path;
+
+  var context = iopaContextFactory.createRequest(urlStr, method);
+
   context["iopa.Body"] = new iopaStream.OutgoingStream();
-  context.response["iopa.Body"] = new iopaStream.IncomingMessageStream();
   
   //REVERSE STREAMS SINCE SENDING REQUEST (e.g., PUBLISH) BACK ON RESPONSE CHANNEL
   context["server.RawStream"] = originalContext.response["server.RawStream"];
-  context.response["server.RawStream"] = originalContext["server.RawStream"];
-  
-  context["iopa.Body"].on("start", function(){
+
+  context["iopa.Body"].on("start", function () {
     context["server.InProcess"] = true;
   });
-  
-  var that = this; 
-    
-  context["iopa.Body"].on("finish", function(){
-     var ctx = context;
-     ctx["server.InProcess"] = true;
-     return that._clientMessagePipeline(context).then(function(value){
-          iopaContextFactory.dispose(ctx);  
-       that = null;
-       ctx = null;
-       return value;
-     });
+
+  context["server.IsLocalOrigin"] = true;
+  context["server.IsRequest"] = false;
+
+  var that = this;
+
+  context["iopa.Body"].on("finish", function () {
+    var ctx = context;
+    ctx["server.InProcess"] = true;
+    return that._clientMessagePipeline(context).then(function (value) {
+      iopaContextFactory.dispose(ctx);
+      that = null;
+      ctx = null;
+      return value;
+    });
   });
-  
+
   return context;
 };
 
@@ -234,10 +231,17 @@ TcpServer.prototype.createResponseRequest = function TcpServer_createResponseReq
  * @public
  */
 TcpServer.prototype.close = function TcpServer_close() {
-   for (var key in this._connections)
-      this._connections[key].destroy();
-      
-   return this._tcp.closeAsync();
+  if (this._tcpClient)
+    this._tcpClient.close();
+
+  for (var key in this._connections)
+    this._connections[key].destroy();
+
+
+  this._tcp.close();
+  this._tcpClient = undefined;
+  this._tcp = undefined;
+  return Promise.resolve(null);
 };
 
 module.exports = TcpServer;
